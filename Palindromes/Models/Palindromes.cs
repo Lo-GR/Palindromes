d4712f620613b1e613913cb328989f15efad7a33
Lo-GR <diamondintheroth@gmail.com>
2021-02-23 14:51:38 -0800

    finish application
    Co-Authored by Tien Nguyen <tien96ng@github.com>

--- Palindromes/Models/Palindromes.cs
+++ Palindromes/Models/Palindromes.cs
@@ -1,12 +1,35 @@
 using System;
 
-namespace Excercise
+namespace PalindromeSpace.Models
 {
   public class Palindrome
   {
     public static bool isPalindrome(string str)
     {
-      return false;
+      string firstHalf, secondHalf;
+      
+      if(str.Length % 2 == 0){
+        firstHalf = str.Substring(0, str.Length/2);
+        secondHalf = str.Substring(str.Length/2);
+      }
+      else
+      {
+        firstHalf = str.Substring(0, str.Length/2 + 1);
+        secondHalf = str.Substring(str.Length/2);
+      }
+      string reverseHalf = "";
+      for (int index = secondHalf.Length - 1; index >= 0; index--)
+      {
+        reverseHalf += secondHalf[index];
+      }
+      if (firstHalf.ToLower() == reverseHalf.ToLower())
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
     }
   }
 }
--- Palindromes/Program.cs
+++ Palindromes/Program.cs
@@ -1,12 +1,23 @@
 using System;
+using PalindromeSpace.Models;
 
-namespace Excercise {
+namespace PalindromeSpace {
   public class Program {
     public static void Main() {
       Console.Write("Please Enter in any word, phrase, number, or other sequence of characters: ");
       string userInput = Console.ReadLine();
-      // bool palindromeChecker = Palindrome.CheckInput(userInput);
-      // Console.WriteLine(palindromeChecker);
+      bool ans = Palindrome.isPalindrome(userInput);
+      if(ans)
+      {
+        Console.BackgroundColor = ConsoleColor.Green;
+        Console.WriteLine(userInput + " is indeed a Palindrome");
+      }
+      else
+      {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.WriteLine(userInput + " is not a Palindrome, sorry guy.");
+      }
+      Console.ResetColor();
     }
   }
 }
